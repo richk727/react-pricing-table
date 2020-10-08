f8e4c315a78e6dd7ba3d0aa1aaf369a77ea120e8
Richard Kaye <richk727@gmail.com>
2020-10-09 00:03:33 +0100

    Added price plan data

---
+++ src/PricePlans.js
@@ -0,0 +1,77 @@
+export const PricePlans = [
+  {
+    'plan-id': 1,
+    title: 'Essential',
+    'annual-monthly-price': '9.99',
+    'monthly-price': '12.99',
+    'button-text': 'Get Caked',
+    features: [
+      {
+        'feature-text': '100 Cakes a month',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': '2 Eaters',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Unlimited sprinkles',
+        'feature-icon': 'tick',
+      },
+    ],
+  },
+  {
+    'plan-id': 2,
+    title: 'Professional',
+    'annual-monthly-price': '39.99',
+    'monthly-price': '49.99',
+    'button-text': 'Get Caked',
+    features: [
+      {
+        'feature-text': '500 Cakes a month',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': '8 Eaters',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Unlimited sprinkles',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Side cookies',
+        'feature-icon': 'plus',
+      },
+    ],
+  },
+  {
+    'plan-id': 3,
+    title: 'Enterprise',
+    'annual-monthly-price': '79.99',
+    'monthly-price': '99.99',
+    'button-text': 'Get Caked',
+    features: [
+      {
+        'feature-text': '999 Cakes a month',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Unlimited Eaters',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Unlimited sprinkles',
+        'feature-icon': 'tick',
+      },
+      {
+        'feature-text': 'Side cookies',
+        'feature-icon': 'plus',
+      },
+      {
+        'feature-text': 'Additional Professional Icing',
+        'feature-icon': 'plus',
+      },
+    ],
+  },
+];
