8d8afec74b161fd325c990f4b234eb7d206d2dc0
Richard Kaye <richk727@gmail.com>
2020-10-10 01:26:06 +0100

    Updates

---
+++ precache-manifest.3188035f9fd10f03e202d08dc83c749f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5631fb3f475dee2fb83c1f2a57365662",
+    "url": "/react-pricing-table/index.html"
+  },
+  {
+    "revision": "82f743d4159dd7add8f5",
+    "url": "/react-pricing-table/static/css/main.0b132b47.chunk.css"
+  },
+  {
+    "revision": "9ce7539baa071b73d606",
+    "url": "/react-pricing-table/static/js/2.c36d5dbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pricing-table/static/js/2.c36d5dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f743d4159dd7add8f5",
+    "url": "/react-pricing-table/static/js/main.d55e9866.chunk.js"
+  },
+  {
+    "revision": "6e19d86098a2ad799fa7",
+    "url": "/react-pricing-table/static/js/runtime-main.705d5015.js"
+  },
+  {
+    "revision": "a04be9ce0eda7f32ca899e012512deb6",
+    "url": "/react-pricing-table/static/media/add-sharp.a04be9ce.svg"
+  },
+  {
+    "revision": "4232211eefad1f1925d83667f3b353f8",
+    "url": "/react-pricing-table/static/media/checkmark-sharp.4232211e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/richk727/react-pricing-table/precache-manifest.17c5e57293967a0d35d4f8f1fd4d4de5.js"
+  "/react-pricing-table/precache-manifest.3188035f9fd10f03e202d08dc83c749f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/richk727/react-pricing-table/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pricing-table/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
